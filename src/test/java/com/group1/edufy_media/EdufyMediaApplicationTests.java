2ea7f812d588ad483eebcaced46aaec63f3a6d8c
Mikael Eriksson <m.eriksson@gmx.com>
2023-10-03 11:27:55 +0200

    Testing is working in the project with Junit. There will be more testing but this is the DoD I set.

--- src/test/java/com/group1/edufy_media/EdufyMediaApplicationTests.java
+++ src/test/java/com/group1/edufy_media/EdufyMediaApplicationTests.java
@@ -1,13 +1,77 @@
 package com.group1.edufy_media;
 
+import com.group1.edufy_media.Model.*;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class EdufyMediaApplicationTests {
 
+    //@Autowired
+    //private Controller controllerName
+
+    //@Autowired
+    //private Service serviceName
+
+    @DisplayName("Album.class is not null")
     @Test
-    void contextLoads() {
+    void albumClassLoaded() {
+        assertThat(Album.class).isNotNull();
     }
 
+    @DisplayName("Artist class is not null.")
+    @Test
+    void artistClassLoaded() {
+        assertThat(Artist.class).isNotNull();
+    }
+    @DisplayName("ContentContributor interface is not null.")
+    @Test
+    void contentContributorInterfaceLoaded() {
+        assertThat(ContentContributor.class).isNotNull();
+    }
+    @DisplayName("Genre class is not null.")
+    @Test
+    void genreClassLoaded() {
+        assertThat(Genre.class).isNotNull();
+    }
+    @DisplayName("Media abstract class is not null.")
+    @Test
+    void mediaAbstractClassLoaded() {
+        assertThat(Media.class).isNotNull();
+    }
+    @DisplayName("MediaType class is not null.")
+    @Test
+    void mediaTypeClassLoaded() {
+        assertThat(MediaType.class).isNotNull();
+    }
+    @DisplayName("Podcast class is not null.")
+    @Test
+    void podcastClassLoaded() {
+        assertThat(Podcast.class).isNotNull();
+    }
+    @DisplayName("PodcastCreator class is not null.")
+    @Test
+    void podcastCreatorClassLoaded() {
+        assertThat(PodcastCreator.class).isNotNull();
+    }
+    @DisplayName("Song class is not null.")
+    @Test
+    void songClassLoaded() {
+        assertThat(Song.class).isNotNull();
+    }
+    @DisplayName("Video class is not null.")
+    @Test
+    void videoClassLoaded() {
+        assertThat(Video.class).isNotNull();
+    }
+    @DisplayName("VideoCreator class is not null.")
+    @Test
+    void videoCreatorClassLoaded() {
+        assertThat(VideoCreator.class).isNotNull();
+    }
 }
