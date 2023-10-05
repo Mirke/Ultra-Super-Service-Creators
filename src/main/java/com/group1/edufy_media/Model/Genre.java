ef012b6ea6e141fb92dd99c2e7a597680cbd2a55
Mikael Eriksson <m.eriksson@gmx.com>
2023-10-05 15:19:37 +0200

    Added getter and setter to genre file and now the service works.

--- src/main/java/com/group1/edufy_media/Model/Genre.java
+++ src/main/java/com/group1/edufy_media/Model/Genre.java
@@ -5,9 +5,28 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "genre")
 public class Genre {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
     private int id;
+
     @Column(name = "genre_name")
     private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
