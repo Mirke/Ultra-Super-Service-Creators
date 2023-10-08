4fc6863cbfdade346e7240d993d4c1b1b2855c01
Mikael Eriksson <m.eriksson@gmx.com>
2023-10-08 16:07:34 +0200

    Now the project has basic sorted list of Genre and MediaType from parent Media. Cleaned some.

--- src/main/java/com/group1/edufy_media/Repositories/MediaRepository.java
+++ src/main/java/com/group1/edufy_media/Repositories/MediaRepository.java
@@ -2,11 +2,17 @@ package com.group1.edufy_media.Repositories;
 
 import com.group1.edufy_media.Model.Media;
 import com.group1.edufy_media.Model.Podcast;
+import com.group1.edufy_media.Model.Song;
+import com.group1.edufy_media.Model.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MediaRepository extends JpaRepository<Media,Integer> {
+
 }
