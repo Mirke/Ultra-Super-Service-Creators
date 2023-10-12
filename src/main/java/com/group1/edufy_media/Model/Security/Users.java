fb361297ae201af973f18b026d08cee9d3bab49f
Moussa Zalzala <moussa.zalzala@hotmail.com>
2023-10-12 08:30:48 +0200

    Started merging everything related to "User"

--- src/main/java/com/group1/edufy_media/Model/Security/Users.java
+++ src/main/java/com/group1/edufy_media/Model/Security/Users.java
@@ -1,6 +1,11 @@
 package com.group1.edufy_media.Model.Security;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.group1.edufy_media.Model.GenrePreference;
+import com.group1.edufy_media.Model.PlayedSong;
 import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -16,13 +21,23 @@ public class Users {
   @Column(name = "enabled", nullable = false)
   private boolean active;
 
+  @OneToMany (mappedBy = "user" ,cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH, CascadeType.PERSIST, CascadeType.REMOVE})
+  @JsonIgnore
+  private List<GenrePreference> genrePreferences;
+
+  @OneToMany (mappedBy = "user" ,cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH, CascadeType.PERSIST, CascadeType.REMOVE})
+  @JsonIgnore
+  private List <PlayedSong> playedSongList;
+
   public Users() {
   }
 
-  public Users(String username, String password, boolean active) {
+  public Users(String username, String password, boolean active, List<GenrePreference> genrePreferences, List<PlayedSong> playedSongList) {
     this.username = username;
     this.password = password;
     this.active = active;
+    this.genrePreferences = genrePreferences;
+    this.playedSongList = playedSongList;
   }
 
   public String getUsername() {
@@ -45,7 +60,23 @@ public class Users {
     return active;
   }
 
-  public void setActive(boolean enabled) {
-    this.active = enabled;
+  public void setActive(boolean active) {
+    this.active = active;
+  }
+
+  public List<GenrePreference> getGenrePreferences() {
+    return genrePreferences;
+  }
+
+  public void setGenrePreferences(List<GenrePreference> genrePreferences) {
+    this.genrePreferences = genrePreferences;
+  }
+
+  public List<PlayedSong> getPlayedSongList() {
+    return playedSongList;
+  }
+
+  public void setPlayedSongList(List<PlayedSong> playedSongList) {
+    this.playedSongList = playedSongList;
   }
 }
--- src/main/java/com/group1/edufy_media/Services/Security/UserServiceImpl.java
+++ src/main/java/com/group1/edufy_media/Services/Security/UserServiceImpl.java
@@ -1,8 +1,10 @@
 package com.group1.edufy_media.Services.Security;
 
+import com.group1.edufy_media.Exceptions.ResourceNotFoundException;
 import com.group1.edufy_media.Model.GenrePreference;
 import com.group1.edufy_media.Model.PlayedSong;
 import com.group1.edufy_media.Model.Security.Users;
+import com.group1.edufy_media.Model.Song;
 import com.group1.edufy_media.Repositories.GenrePreferenceRepository;
 import com.group1.edufy_media.Repositories.PlayedSongRepository;
 import com.group1.edufy_media.Repositories.Security.UserRepository;
@@ -10,6 +12,7 @@ import com.group1.edufy_media.Repositories.SongRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -37,6 +40,31 @@ public class UserServiceImpl implements UserService{
   @Override
   public List<PlayedSong> findPlayedSongsByUser(Users user) {
     return playedSongRepository.findPlayedSongsByUser(user);
+  }
+
+  @Override
+  public List<Song> getSongsNotPlayedByUser(String username) {
+    Users user = userRepository.findByUsername(username);
+    // A users played Songs:
+    List<PlayedSong> playedSongsByUser = user.getPlayedSongList();
+
+    // Song-object List of what the user has listened to.
+    List<Song> playedSongsByUserList = new ArrayList<>();
+
+    // Fill the list with the Song objects from the first list.
+    for(PlayedSong playedSong : playedSongsByUser) {
+      playedSongsByUserList.add(playedSong.getSong());
+    }
+
+    // Create a list of all the songs in the repo:
+
+    List<Song> allSongs = songRepository.findAll();
+
+    // Strip down the allSongs list from all the songs that the user has played to leave songs never played:
+
+    allSongs.removeAll(playedSongsByUserList);
+
+    return allSongs;
   }
 
 
